2778d27b3097239d22c8793e246c36d8dea846aa
devImexindo <dev.imexindo@gmail.com>
2025-09-02 17:32:00 +0700

    core

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -28,7 +28,9 @@
 <!-- Helpers -->
 <script src="{{ asset('sneat/assets/vendor/js/helpers.js') }}"></script>
 <script src="{{ asset('sneat/assets/js/config.js') }}"></script>
-<link rel="stylesheet" href="https://a.omappapi.com/app/js/api.min.css">
+{{-- <script src="{{ asset('assets/css/core.css') }}"></script> --}}
+
+{{-- <link rel="stylesheet" href="https://a.omappapi.com/app/js/api.min.css"> --}}
 <link rel="stylesheet" href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/assets/vendor/css/core.css">
-<link rel="stylesheet" href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/assets/css/demo.css">
+{{-- <link rel="stylesheet" href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/assets/css/demo.css"> --}}
 
